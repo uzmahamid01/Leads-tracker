7d3e4b7c4a21497213e59248039856c924502c52
uzmahamid01 <uzma_hamid@tamu.edu>
2024-05-28 23:49:08 -0500

    added changes

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-let myLeads = []
-//let and const to declare variables
-//const cant be reassigned 
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("list-el")
+let myLeads = [];
+let oldLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("list-el");
+const deleteBtn = document.getElementById("delete-btn");
 
-console.log(ulEl)
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    //clear the input field
-    inputEl.value = ""
-    renderLeads()
-
-})
-
-
-function renderLeads() {
-    let listItems = ""
-
-    for (let i = 0; i < myLeads.length; i++) {
-        // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-        //create the element
-        // const li = document.createElement("li")
-        // li.textContent = myLeads[i]
-        // ulEl.append(li)
-        // listItems += `<a target='_black' href="${myLeads[i]}"><li>${myLeads[i]}</li></a>`;  
-        listItems += `
-        <li>
-            <a target='_blank' href="${myLeads[i]}">${myLeads[i]}</a>
-        </li>`; 
-
-    }
-    ulEl.innerHTML = listItems
-    
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
 }
 
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href="${leads[i]}">${leads[i]}</a>
+        </li>`;
+    }
+    ulEl.innerHTML = listItems;
+}
 
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
 
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
